5c2882587c1c01dc1601ef0ab69aaf3c2bac36f8
asmaboushaba8-creator <asmaboushaba8@gmail.com>
2026-06-24 17:07:25 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import StartNowApp from './StartNowApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StartNowApp />
     <App />
   </React.StrictMode>
 )
